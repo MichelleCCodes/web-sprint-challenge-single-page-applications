dec8e526e78136ea8c92916589961aed75abcc2d
Michelle Chen <mchen0062@gmail.com>
2020-12-18 15:44:10 -0800

    Created html for form

--- src/Form.js
+++ src/Form.js
@@ -2,26 +2,45 @@ import React from 'react';
 
 export default function Form () {
 return (
-    <form className="form-container">
+<form className="form-container">
 <div className="pizza-form">
 
+<label>
 <h3>Choice of Size</h3>
 <p>Required</p>
 {/* DROPDOWN */}
+<select>
+    <option value="">select size</option>
+    <option value="small">small</option>
+    <option value="medium">medium</option>
+    <option value="large">large</option>
+</select>
+</label>
+
+<label>
 <h3>Choice of Sauce</h3>
 <p>Required</p>
 {/* RADIO */}
+<div><input type="radio" name="sauce" value="original"/>Original Red</div>
+<div><input type="radio" name="sauce" value="garlic" />Garlic Ranch</div>
+<div><input type="radio" name="sauce" value="bbq" />BBQ Sauce</div>
+<div><input type="radio" name="sauce" value="spinach" />Spinach Alfredo</div>
+</label>
+
+<label>
 <h3>Add Toppings</h3>
 <p>Choose up to 10</p>
 {/* checkboxes */}
-<h3>Choice of Choice of Substitute</h3>
-<p>Choose up to 1</p>
-{/*toggle bar*/}<span>Gluten Free Crust (+$1.00)</span>
-<h3>Special Instructions</h3>
-{/*Input text placeholder: Anything else you'd like to add?*/}
-{/* QUANTITY BAR + Add to Order (submit button)*/}
-</div>
+<input type="checkbox" name="pepperoni"/>pepperoni
+<input type="checkbox" name="sausage"/>sausage
+<input type="checkbox" name="onion"/>onion
+<input type="checkbox" name="green-pepper"/>green pepper
+<input type="checkbox" name="black-olives"/>black olives
+<input type="checkbox" name="artichoke-hearts"/>artichoke hearts
+<input type="checkbox" name="three-cheese"/>three cheese
+</label>
 
-    </form>
+</div>
+</form>
 ); 
 }
